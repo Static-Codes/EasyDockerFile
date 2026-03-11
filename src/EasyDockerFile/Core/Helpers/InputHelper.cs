8fe22f6142843ca50a3ea39b41aa563921ef748a
Static <210101922+Static-Codes@users.noreply.github.com>
2026-03-11 14:27:31 -0400

    Added InputHelper.AskForInput

--- src/EasyDockerFile/Core/Helpers/InputHelper.cs
+++ src/EasyDockerFile/Core/Helpers/InputHelper.cs
@@ -37,6 +37,15 @@ public static class InputHelper
         return AnsiConsole.Prompt(prompt);
     }
 
+    public static string AskForInput(string message)
+    {
+        var style = new Style(decoration: Decoration.Bold);
+        var prompt = new SelectionPrompt<string>() { SearchEnabled = true }
+        .HighlightStyle(style)
+        .Title(message);
+        return AnsiConsole.Prompt(prompt);
+    }
+
     public static IEnumerable<string> MakeInputMenu(IEnumerable<string> options) {
         return options.Append(ExitChoice); 
     }
